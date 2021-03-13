fb8cb3da92e8dd8a81ec782c78adaa520b862bd5
MansiSNawlani <mansishankarnawlani@gmail.com>
2021-03-13 13:55:28 +0530

    MongoDB, Redux implementaion,Initial Authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.json(products);
-});
-router.get('/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js';
+
+router.route('/').get(getProducts);
+
+router.route('/:id').get(getProductById);
 
 export default router;
--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+
 const Rating = ({ value, text, color }) => {
   return (
     <div className='rating'>
